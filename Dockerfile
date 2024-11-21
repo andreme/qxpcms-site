5307da5670ed05d7b02f8164ef4d9e729ad880db
Andre Meyer <amozaq@gmail.com>
2024-11-21 13:31:42 -0800

    yarn -> pnpm

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM public.ecr.aws/lambda/nodejs:20.2024.11.19.18
 
-RUN npm install -g yarn
+RUN corepack enable && \
+	corepack prepare pnpm@8.14.1 --activate
 
 RUN dnf install unzip -y && \
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
--- bin/generate-deploy-site.sh
+++ bin/generate-deploy-site.sh
@@ -5,8 +5,8 @@ TASK_ID=$(echo $ECS_CONTAINER_METADATA_URI | cut -d "/" -f 5 | cut -d "-" -f 1)
 
 aws events put-events --cli-input-json '{"Entries":[{"Source":"qxpcms.'"$PROJECT_NAME"'","Detail":"{\"action\":\"site-deployment.start\",\"siteDeploymentId\":\"'"$SITE_DEPLOYMENT_ID"'\",\"siteDeploymentLogId\":\"'"$TASK_ID"'\"}","EventBusName":"'"$PROJECT_NAME"'-events","DetailType":"cms.log.system"}]}' > /dev/null
 
-yarn build
+pnpm build
 
-yarn deploy
+pnpm deploy
 
 aws events put-events --cli-input-json '{"Entries":[{"Source":"qxpcms.'"$PROJECT_NAME"'","Detail":"{\"action\":\"site-deployment.finished\",\"siteDeploymentId\":\"'"$SITE_DEPLOYMENT_ID"'\"}","EventBusName":"'"$PROJECT_NAME"'-events","DetailType":"cms.log.system"}]}' > /dev/null
